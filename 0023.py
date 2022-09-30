3f091abac5ba6a466fb9d00c6034947ac6877f62
victor giordani <victor.giordani@protonmail.com>
2022-09-30 20:54:31 +0200

    NEED TO BE IMPROVED

---
+++ 0023.py
@@ -0,0 +1,52 @@
+
+def d(n):
+    somme = 0
+    for i in range(1, n// 2 +1):
+        if n % i == 0:
+            somme += i
+    return somme
+
+
+
+#n < d(n) = abundant number
+#n = d(n) = perfect number
+#n > d(n) = deficient number
+
+abundant_nb = []
+
+def generating_list_of_abundant_number(i): #until n
+    for n in range(i):
+        if (n < d(n)):
+            abundant_nb.append(n)
+            print(n)
+
+
+generating_list_of_abundant_number(28123)
+print(abundant_nb)
+print(d(7874))
+
+def is_the_sum_of_two_abundants(nb):
+    for nb1 in abundant_nb:
+        for nb2 in abundant_nb:
+            if (nb2 + nb1 > nb):
+                break
+            if (nb2 + nb1 == nb):
+                return (1)
+    print(nb)
+    return (0)
+                
+
+
+result = 0
+
+for i in range (28123):
+    if is_the_sum_of_two_abundants(i) == 0:
+        result += i
+
+print(result)
+
+        
+                
+    
+                
+            
